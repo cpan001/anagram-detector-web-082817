e3d62a3c63e4d420226a8d625e29b90bee275c3e
cpan001 <github email address>
2017-09-01 16:03:00 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_reader :word_str
+
+  def initialize(word_str)
+    @word_str = word_str
+  end
+
+  def match(array)
+    word = self.word_str.split("").sort.join
+    array.select do |a|
+      array_word = a.split("").sort.join
+      word.match(array_word)
+    end
+  end
+
+end
